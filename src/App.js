2915a07e1281909944ed99b0a0962686d819d569
Thanapech Tunwanarutsakul <thanapech.t@gmail.com>
2019-07-22 18:20:59 +0700

    :wrench: to-do-list project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: [],
+      text: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value,
+    })
+  }
+
+  handleSubmit = e => {
+    if (this.state.text !== '') {
+      this.setState({
+        list: [...this.state.list, this.state.text],
+        text: '',
+      })
+    }
+  }
+
+  handleRemove = index => {
+    let newList = [...this.state.list].filter((val, idx) => idx !== index)
+    this.setState({
+      list: newList,
+    })
+  }
+
+  handleEdit = (text, index) => {
+    let newList = [...this.state.list]
+    newList.splice(index, 1, text)
+    this.setState({
+      list: newList,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>TO DO LIST ! {this.state.list.length}</header>
+        <input
+          onChange={this.handleChange}
+          value={this.state.text}
+          onKeyPress={e => {
+            if (e.charCode === 13) {
+              this.handleSubmit()
+            }
+          }}
+        />
+        <button onClick={this.handleSubmit}>submit</button>
+        {this.state.list &&
+          this.state.list.map((val, index) => (
+            <ToDoList
+              key={index}
+              val={val}
+              index={index}
+              handleRemove={this.handleRemove}
+              handleEdit={this.handleEdit}
+            />
+          ))}
+
+        {this.state.list && this.state.list.map(val => val + ',')}
+      </div>
+    )
+  }
 }
 
-export default App;
+class ToDoList extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      isEdit: false,
+      text: props.val,
+    }
+    this.handleEditStatus = this.handleEditStatus.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleEditStatus = () => {
+    this.setState({
+      isEdit: !this.state.isEdit,
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      text: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div key={this.props.index}>
+        {this.state.isEdit ? (
+          <input onChange={this.handleChange} value={this.state.text} />
+        ) : (
+          this.state.text
+        )}
+        <button
+          onClick={() => {
+            this.handleEditStatus()
+            this.state.isEdit &&
+              this.props.handleEdit(this.state.text, this.props.index)
+          }}>
+          {this.state.isEdit ? 'save' : 'edit'}
+        </button>
+        <button onClick={() => this.props.handleRemove(this.props.index)}>
+          remove
+        </button>
+      </div>
+    )
+  }
+}
+
+export default App
